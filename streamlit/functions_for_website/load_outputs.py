7fa2b067261a6d09ff1413e105c67c7b0d2acc2c
William Bannister <willbanny2@gmail.com>
2023-06-14 22:38:14 +0100

    edited london function

--- streamlit/functions_for_website/load_outputs.py
+++ streamlit/functions_for_website/load_outputs.py
@@ -53,19 +53,52 @@ def load_london_output_df():
 
 
 
-def load_london_gdf_data(district:list):
+def load_london_gdf_data():
+    input_list = ['Barking and Dagenham London Boro',
+ 'Barnet London Boro',
+ 'Bexley London Boro',
+ 'Brent London Boro',
+ 'Bromley London Boro',
+ 'Camden London Boro',
+ 'City and County of the City of London',
+ 'City of Westminster London Boro',
+ 'Croydon London Boro',
+ 'Ealing London Boro',
+ 'Enfield London Boro',
+ 'Greenwich London Boro',
+ 'Hackney London Boro',
+ 'Hammersmith and Fulham London Boro',
+ 'Haringey London Boro',
+ 'Harrow London Boro',
+ 'Havering London Boro',
+ 'Hillingdon London Boro',
+ 'Hounslow London Boro',
+ 'Islington London Boro',
+ 'Kensington and Chelsea London Boro',
+ 'Kingston upon Thames London Boro',
+ 'Lambeth London Boro',
+ 'Lewisham London Boro',
+ 'Merton London Boro',
+ 'Newham London Boro',
+ 'Redbridge London Boro',
+ 'Richmond upon Thames London Boro',
+ 'Southwark London Boro',
+ 'Sutton London Boro',
+ 'Tower Hamlets London Boro',
+ 'Waltham Forest London Boro',
+ 'Wandsworth London Boro']
+
     '''
     loads up the dataframes to be used for the map
     '''
-    bad_df = pd.read_csv(os.path.abspath("outputs/display_bad.csv"))
-    good_df = pd.read_csv(os.path.abspath("outputs/display_gd.csv"))
+    bad_df = pd.read_csv(os.path.abspath("outputs/display_bad_ldn.csv"))
+    good_df = pd.read_csv(os.path.abspath("outputs/display_gd_ldn.csv"))
     carehomes = pd.read_csv(os.path.abspath("outputs/all_carehomes.csv"))
 
 
-    data = good_df[good_df['district_name'].isin([district])][['lat', 'lng', 'metric']].copy()
-    dataBd = bad_df[bad_df['district_name'].isin([district])][['lat', 'lng', 'metric']].copy()
-    care_data = carehomes[carehomes['district_name'].isin([district])][['lat', 'lng']].copy()
-
+    data = good_df[good_df['district_name'].isin(input_list)][['lat', 'lng', 'metric']].copy()
+    dataBd = bad_df[bad_df['district_name'].isin(input_list)][['lat', 'lng', 'metric']].copy()
+    care_data = carehomes[carehomes['district_name'].isin(input_list)][['lat', 'lng']].copy()
     df = pd.DataFrame(data)
     df2 = pd.DataFrame(dataBd)
     df3 = pd.DataFrame(care_data)
