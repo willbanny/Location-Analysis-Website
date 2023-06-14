fce450ae673c3e5236187101980f45b4034c2293
William Bannister <willbanny2@gmail.com>
2023-06-14 21:55:49 +0100

    edits

--- streamlit/pages/London_Analysis.py
+++ streamlit/pages/London_Analysis.py
@@ -30,6 +30,9 @@ lats = labeled_df['lat']
 longs = labeled_df['lng']
 clusters = labeled_df['Labels']
 
+st.dataframe(all_df)
+
+
 input_list = ['Barking and Dagenham London Boro',
  'Barnet London Boro',
  'Bexley London Boro',
@@ -65,7 +68,8 @@ input_list = ['Barking and Dagenham London Boro',
  'Wandsworth London Boro']
 # create multiselect options
 option = st.multiselect("select/deselect London boroughs",
-                        input_list,default=['City and County of the City of London'])
+                        input_list,default=['City and County of the City of London'],
+                        on_change=st.session_state['district'] = input_list)
 
 # set up the website to show Dorset on initializing
 if 'district' not in st.session_state:
